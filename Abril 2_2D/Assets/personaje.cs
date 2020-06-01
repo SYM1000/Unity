734bba8e7b9b3046e092f89946251ee96f72db2a
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2020-05-31 23:50:32 -0700

    2D Game

---
+++ Abril 2_2D/Assets/personaje.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personaje : MonoBehaviour
+{
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float velocidad = h * 5 * Time.deltaTime;
+
+        transform.Translate(velocidad, 0, 0);
+        Debug.Log(velocidad);
+        animator.SetFloat("Velocidad", velocidad);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Colision");
+    }
+}
